fc8924639361274f379197009dc25846a7495864
BaoQiang <mailbaoqiang@gmail.com>
2016-11-08 22:00:29 +0800

    TrainModelThread TrainModel to be complete

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # xiao-c-word2vec
 i want to write all word2vec code this weekend
+
+questions:
+1, what is "windows","sample","classes", in param?
--- word2vec.c
+++ word2vec.c
@@ -337,18 +337,181 @@ void SaveVocad() {
 
 void ReadVocab() {
     long long a, i = 0;
+    char c;
+    char word[MAX_STRING];
+    FILE *fin = fopen(read_vocab_file, "rb");
+    if (fin == NULL) {
+        printf("Vocabulary file not found\n");
+        exit(-1);
+    }
+    for (a = 0; a < vocab_hash_size; a++) {
+        vocab_hash[a] = -1;
+    }
+    vocab_size = 0;
+    while (1) {
+        ReadWord(word, fin);
+        if (feof(fin)) {
+            break;
+        }
+        a = AddWordToVocab(word);
+        fscanf(fin, "%lld\n", &vocab[a].cn, &c);
+        i++;
+    }
+    SortVocab();
+    if (debug_mode > 0) {
+        printf("Vocab size: %lld\n", vocab_size);
+        printf("Words in training file: %lld\n", train_words);
+    }
+    fin = fopen(train_file, "rb");
+    if (fin == NULL) {
+        printf("ERROR: training data file not found!\n");
+        exit(-1);
+    }
+    fseek(fin, 0, SEEK_END);
+    file_size = ftell(fin);
+    fclose(fin);
 }
 
+void InitNet() {
+    long long a, b;
+    unsigned long long next_random = 1;
+//    a = posix_memalign((void **) &syn0, 128, vocab_size * layer1_size * sizeof(real));
+    syn0 = _aligned_malloc(128, vocab_size * layer1_size * sizeof(real));
+    if (syn0 == NULL) {
+        printf("Memory allocation failed\n");
+        exit(-1);
+    }
+    if (hs) {
+        syn1 = _aligned_malloc(128, vocab_size * layer1_size * sizeof(real));
+        if (syn1 == NULL) {
+            printf("Memory allocation failed\n");
+            exit(-1);
+        }
+        for (a = 0; a < vocab_size; a++) {
+            for (b = 0; b < layer1_size; b++) {
+                syn1[a * layer1_size + b] = 0;
+            }
+        }
+    }
+    if (negative > 0) {
+        syn1neg = _aligned_malloc(128, vocab_size * layer1_size * sizeof(real));
+        if (syn1neg == NULL) {
+            printf("Memory allocation failed\n");
+            exit(-1);
+        }
+        for (a = 0; a < vocab_size; a++) {
+            for (b = 0; b < layer1_size; b++) {
+                syn1neg[a * layer1_size + b] = 0;
+            }
+        }
+    }
+    for (a = 0; a < vocab_size; a++) {
+        for (b = 0; b < layer1_size; b++) {
+            next_random = next_random * (unsigned long long) 25214903917 + 11;
+            syn0[a * layer1_size + b] = (((next_random & 0xFFFF) / (real) 65536) - 0.5) / layer1_size;
+        }
+    }
+    CreateBinaryTree();
 
+}
 
+void *TrainModelThread(void *id) {
 
+}
 
+void TrainModel() { }
 
+int ArgPos(char *str, int argc, char **argv) {
+    int a;
+    for (a = 1; a < argc; a++) {
+        if (!strcmp(str, argv[a])) {
+            if (a == argc - 1) {
+                printf("Argument missing for %s\n", str);
+                exit(1);
+            }
+            return a;
+        }
+    }
+    return -1;
+}
 
+int main(int argc, char **argv) {
+    int i;
+    if (argc == 1) {
+        printf("WORD VECTOR estimation toolkit v 0.1c\n\n");
+        printf("Options:\n");
+        printf("Parameters for training:\n");
+        printf("\t-train <file>\n");
+        printf("\t\tUse text data from <file> to train the model\n");
+        printf("\t-output <file>\n");
+        printf("\t\tUse <file> to save the resulting word vectors / word clusters\n");
+        printf("\t-size <int>\n");
+        printf("\t\tSet size of word vectors; default is 100\n");
+        printf("\t-window <int>\n");
+        printf("\t\tSet max skip length between words; default is 5\n");
+        printf("\t-sample <float>\n");
+        printf("\tSet threshold for occurence of words. Thoes that appear with high frequency in the training data\n");
+        printf("\twill be randomly down-sampled; default is 1e-3, useful range is (0,1e-5)\n");
+        printf("\t-hs <int>\n");
+        printf("\t\tUse Hierarchical Softmax; default is 0(not used)\n");
+        printf("\t-negative <int>\n");
+        printf("\t\tNumber of negative examples; default is 5, common values are 3 - 10(0 = not used)\n");
+        printf("\t-threads <int>\n");
+        printf("\t\tUse <int> threads (default 12)\n");
+        printf("\t-iter <int>\n");
+        printf("\t\tRun more training iterations (default 5)\n");
+        printf("\t-min-count <int>\n");
+        printf("\t\tThis will discard words that appear less than <int> times; default is 5\n");
+        printf("\t-alpha <float>\n");
+        printf("\t\tSet the starting learning rate; default is 0.025 for skip-gram and 0.05 for CBOW\n");
+        printf("\t-classes <int>\n");
+        printf("\t\tOutput word classes rather than word vectors; default number of classes is 0 (vector are written)\n");
+        printf("\t-debug <int>\n");
+        printf("\t\tSet the debug mode (default = 2 = more info during training)\n");
+        printf("\t-binary <int>\n");
+        printf("\t\tSave the resulting vectors in binary moded; default is 0(off)\n");
+        printf("\t-save-vocab <file>\n");
+        printf("\t\tThe vocabulary will be saved to <file>\n");
+        printf("\t-read-vocab <file>\n");
+        printf("\t\tThe vocabulary will be read from <file>, not constrcted from the training data\n");
+        printf("\t-cbow <int>\n");
+        printf("\t\tUse the continuous bag of words model; default is 1(use 0 for skip-gram model)\n");
+        printf("\nExamples:\n");
+        printf("./word2vec -train data.txt -output vec.txt -size 200 -window 5 -sample 1e-4 -negative 5 -hs 0 -binary 0 -cbow 1 -iter 3\n\n");
+        return 0;
+    }
+    output_file[0] = 0;
+    save_vocab_file[0] = 0;
+    read_vocab_file[0] = 0;
+    if ((i = ArgPos((char *) "-size", argc, argv)) > 0)layer1_size = atoi(argv[i + 1]);
+    if ((i = ArgPos((char *) "-train", argc, argv)) > 0) strcpy(train_file, argv[i + 1]);
+    if ((i = ArgPos((char *) "-save-vocab", argc, argv)) > 0)strcpy(save_vocab_file, argv[i + 1]);
+    if ((i = ArgPos((char *) "-read-vocab", argc, argv)) > 0)strcpy(read_vocab_file, argv[i + 1]);
+    if ((i = ArgPos((char *) "-debug", argc, argv)) > 0)debug_mode = atof(argv[i + 1]);
+    if ((i = ArgPos((char *) "-binary", argc, argv)) > 0)binary = atof(argv[i + 1]);
+    if ((i = ArgPos((char *) "-cbow", argc, argv)) > 0)cbow = atof(argv[i + 1]);
+    if (cbow) alpha = 0.05;
+    if ((i = ArgPos((char *) "-alpha", argc, argv)) > 0)alpha = atof(argv[i + 1]);
+    if ((i = ArgPos((char *) "-output", argc, argv)) > 0)strcpy(output_file, argv[i + 1]);
+    if ((i = ArgPos((char *) "-window", argc, argv)) > 0)window = atof(argv[i + 1]);
+    if ((i = ArgPos((char *) "-sample", argc, argv)) > 0)sample = atof(argv[i + 1]);
+    if ((i = ArgPos((char *) "-hs", argc, argv)) > 0)hs = atof(argv[i + 1]);
+    if ((i = ArgPos((char *) "-negative", argc, argv)) > 0)negative = atof(argv[i + 1]);
+    if ((i = ArgPos((char *) "-threads", argc, argv)) > 0)num_threads = atof(argv[i + 1]);
+    if ((i = ArgPos((char *) "-iter", argc, argv)) > 0)iter = atof(argv[i + 1]);
+    if ((i = ArgPos((char *) "-min-count", argc, argv)) > 0)min_count = atof(argv[i + 1]);
+    if ((i = ArgPos((char *) "-classes", argc, argv)) > 0)classes = atof(argv[i + 1]);
 
-
-
-
+    vocab = (struct vocab_word *) calloc(vocab_max_size, sizeof(struct vocab_word));
+    vocab_hash = (int *) calloc(vocab_hash_size, sizeof(int));
+    expTable = (real *) malloc((EXP_TABLE_SIZE + 1) * sizeof(real));
+    for (i = 0; i < EXP_TABLE_SIZE; ++i) {
+        expTable[i] = exp((i / (real) EXP_TABLE_SIZE * 2 - 1) * MAX_EXP); // pre computer the exp table
+        expTable[i] = expTable[i] / (expTable[i] + 1);
+    }
+    TrainModel();
+    return 0;
+}
 
 
 
